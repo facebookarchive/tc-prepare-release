42a60f3a065412be53f8e81428da39294ddfac8e
Nantas Nardelli <nantas.nardelli@gmail.com>
2017-03-18 18:34:03 +0000

    Add branch creation bit
    Every time someone looks at the exception hack, Skynet becomes stronger and
    angrier.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import subprocess
 from distutils import dir_util
@@ -5,6 +6,7 @@ from distutils import dir_util
 import argparse
 import random
 import logging
+import traceback
 
 
 class cd:
@@ -22,7 +24,7 @@ class cd:
         if self.with_backup:
             self.__backup_dir()
 
-    def __exit__(self, etype, value, traceback):
+    def __exit__(self, etype, value, tb):
         os.chdir(self.savedPath)
 
     def __backup_dir(self):
@@ -31,6 +33,12 @@ class cd:
         logging.info('Backing up directory into %s' % dest)
         dir_util.copy_tree(self.newPath, dest)
         logging.info('Done')
+
+
+def has_develop():
+    logging.debug('Checking whether develop branch exists...')
+    results = subprocess.check_output(['git', 'branch'])
+    return 'develop' in results.decode('utf-8')
 
 
 def check_repo():
@@ -51,7 +59,15 @@ def pull_repo():
 
 
 def create_new_branch():
-    pass
+    logging.info('Creating branch new_release...')
+    try:
+        branch_name = 'new_release'
+        subprocess.check_output(['git', 'checkout', '-b', branch_name])
+    except:
+        # What could possibly go wrong v2.0
+        branch_name = 'new_release_%05d' % random.randint(0, 99999)
+        subprocess.check_output(['git', 'checkout', '-b', branch_name])
+    logging.info('Created branch %s', branch_name)
 
 
 def update_versions():
@@ -78,14 +94,9 @@ def main():
         type=str,
         help='TorchCraft directory path (relative or absoute)')
     parser.add_argument(
-        '-d',
-        action='store_true',
-        help='Print debug information'
-    )
+        '-d', action='store_true', help='Print debug information')
     parser.add_argument(
-        '-b',
-        action='store_true',
-        help='Backup TorchCraft directory')
+        '-b', action='store_true', help='Backup TorchCraft directory')
     args = parser.parse_args()
 
     if args.d:
@@ -93,14 +104,27 @@ def main():
     else:
         logging.basicConfig(level=logging.INFO)
 
-    with cd(args.tcdir, args.b):
-        check_repo()
-        pull_repo()
-        create_new_branch()
-        update_versions()
-        make_commit()
-        push()
-        create_zip()
+    good_repo = False
+    try:
+        with cd(args.tcdir, args.b):
+            # Side effects ftw
+            has_develop()
+            good_repo = True
+            check_repo()
+            pull_repo()
+            create_new_branch()
+            update_versions()
+            make_commit()
+            push()
+            create_zip()
+    except:
+        if good_repo:
+            # HACK HACK HACK If you are wondering why we do this, it's because
+            # it makes debugging / development easier. I'm sorry.
+            with cd(args.tcdir, False):
+                subprocess.check_output(['git', 'checkout', 'develop'])
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, exc_traceback)
 
 
 if __name__ == '__main__':
